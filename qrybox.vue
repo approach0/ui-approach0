0de2c709769b4dcc691508068c20750c14c48e96
t-k- <clock126@126.com>
2020-11-16 23:50:03 +0800

    qrybox.vue css abstraction.

--- qrybox.vue
+++ qrybox.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="p-grid p-d-flex p-jc-center">
-    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-card" style="height: 3rem; margin: 0.5rem 0 0.5rem 0; max-width: 600px; flex-grow: 1">
-      <input placeholder="Enter query keywords here, type $ for math formula." class="qrybox-editor" :value="editing_val" @input="$emit('update:editing_val', $event.target.value)">
+    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-card input-wrapper input-stretch">
+      <input placeholder="Enter query keywords here, type $ for math formula."
+       class="qrybox-editor" :value="editing_val" @input="$emit('update:editing_val', $event.target.value)">
     </div>
     <div class="p-d-flex p-lg-fixed p-md-12 p-sm-12" style="width: 150px;">
-      <Button label="Search" class="p-button-raised a0-color" style="width: 100%;" @click="onClickSearch()"/>
+      <Button label="Search" class="p-button-raised a0-color" style="width: 100%" @click="onClickSearch()"/>
     </div>
   </div>
 
   <div class="p-grid p-d-flex p-jc-center">
-    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-jc-end" style="max-width: 600px; flex-grow: 1">
+    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-jc-end input-stretch">
       <Button label="example" icon="las la-lightbulb" class="p-button-secondary p-button-text p-button-sm"/>
       <Button label="clear" icon="las la-times" class="p-button-secondary p-button-text p-button-sm"/>
     </div>
@@ -45,6 +46,16 @@ export default {
 </script>
 
 <style>
+.input-wrapper {
+  height: 3rem;
+  margin: 0.5rem 0 0.5rem 0;
+}
+
+.input-stretch {
+  max-width: 600px;
+  flex-grow: 1 !important;
+}
+
 .a0-color {
   background-color: #54c6c0 !important;
 }
