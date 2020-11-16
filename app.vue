42d119efee54cee5e895ba0d2b234f3d9c1abbe4
t-k- <clock126@126.com>
2020-11-16 15:59:21 +0800

    make a Vue component for qrybox.

--- app.vue
+++ app.vue
@@ -1,19 +1,19 @@
 <template>
-  <Toolbar class="topbar">
-    <template v-slot:left>
+  <div class="topbar p-component p-toolbar p-grid">
+    <div class="p-d-flex p-ai-center p-md-6 p-sm-12 p-xsm-12">
       <img :src="logo32" class="p-mx-3"/>
       <div class="p-d-flex p-flex-column">
         <span class="logo-text">Approach Zero</span>
         <div class="logo-text">A math-aware search engine.</div>
       </div>
-    </template>
+    </div>
 
-    <template v-slot:right>
+    <div class="p-d-flex p-jc-end p-ai-center p-md-6 p-sm-12 p-xsm-12 p-my-4">
       <i class="las la-sun"></i>
       <InputSwitch v-model="nightTheme" class="p-mx-3"/>
       <i class="las la-moon"></i>
-    </template>
-  </Toolbar>
+    </div>
+  </div>
 
   <css-doodle class="doodle rellax" data-rellax-speed="6">
     @grid: 40x2 / 60%;
@@ -28,25 +28,10 @@
     );
   </css-doodle>
 
-  <div class="p-d-flex p-ai-center p-jc-center p-flex-column" style="height: 80vh;">
-    <div class="p-grid p-d-flex p-jc-center" style="width: 50%">
-      <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-card" style="height: 3rem; margin: 0.5rem 0 0.5rem 0; max-width: 600px; flex-grow: 1">
-        <input placeholder="Enter query keywords here, type $ for math formula." class="qrybox-editor">
-      </div>
-      <div class="p-d-flex p-lg-fixed p-md-12 p-sm-12" style="width: 150px;">
-        <Button label="Search" class="p-button-raised a0-color" style="width: 100%;"/>
-      </div>
+  <div class="p-d-flex p-jc-center p-ai-end" style="height: 50vh;">
+    <div style="width: 90%">
+      <qrybox/>
     </div>
-
-    <div class="p-grid p-d-flex p-jc-center" style="width: 50%">
-      <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-jc-end" style="max-width: 600px; flex-grow: 1">
-        <Button label="example" icon="las la-lightbulb" class="p-button-secondary p-button-text p-button-sm"/>
-        <Button label="clear" icon="las la-times" class="p-button-secondary p-button-text p-button-sm"/>
-      </div>
-      <div class="p-d-flex p-lg-fixed p-md-12 p-sm-12" style="width: 150px;">
-      </div>
-    </div>
-
   </div>
 
   <div style="background-color: grey; height: 1800px; width: 2px"></div>
@@ -55,8 +40,10 @@
 
 <script>
 const axios = require('axios')
+import qrybox from './qrybox.vue'
 
-module.exports = {
+export default {
+  components: { qrybox },
   mounted: function() {
     this.attachDefaultTheme()
     new Rellax('.rellax')
@@ -131,7 +118,6 @@ body {
 .topbar {
   position: sticky;
   top: 0;
-  width: 100%;
   z-index: 999;
 }
 
@@ -139,7 +125,7 @@ body {
 .doodle {
   position: absolute;
   right: 10%;
-  bottom: 0;
+  bottom: -100px;
   z-index: -1;
 }
 
@@ -152,16 +138,9 @@ body {
   background-color: #54c6c0 !important;
 }
 
-.qrybox-editor {
-  caret-color: var(--text-color);
-  width: 100%;
-  height: 100%;
-  border: none;
-  outline: none;
-  background-color: var(--surface-e);
-}
-
-.p-button {
-  max-height: 3rem;
+@media screen and (max-width: 576px) {
+  .p-xsm-12 {
+    width: 100%;
+  }
 }
 </style>
---
+++ qrybox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="p-grid p-d-flex p-jc-center">
+    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-card" style="height: 3rem; margin: 0.5rem 0 0.5rem 0; max-width: 600px; flex-grow: 1">
+      <input placeholder="Enter query keywords here, type $ for math formula." class="qrybox-editor">
+    </div>
+    <div class="p-d-flex p-lg-fixed p-md-12 p-sm-12" style="width: 150px;">
+      <Button label="Search" class="p-button-raised a0-color" style="width: 100%;"/>
+    </div>
+  </div>
+
+  <div class="p-grid p-d-flex p-jc-center">
+    <div class="p-d-flex p-lg-1 p-md-12 p-sm-12 p-p-2 p-jc-end" style="max-width: 600px; flex-grow: 1">
+      <Button label="example" icon="las la-lightbulb" class="p-button-secondary p-button-text p-button-sm"/>
+      <Button label="clear" icon="las la-times" class="p-button-secondary p-button-text p-button-sm"/>
+    </div>
+    <div class="p-d-flex p-lg-fixed p-md-12 p-sm-12" style="width: 150px;">
+      <!-- Placeholder -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function() {
+  },
+
+  watch: {
+  },
+
+  data: function() {
+    return {
+      inputBox: '',
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style>
+.a0-color {
+  background-color: #54c6c0 !important;
+}
+
+.qrybox-editor {
+  caret-color: var(--text-color);
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  background-color: var(--surface-e);
+}
+</style>
