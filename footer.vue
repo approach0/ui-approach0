f2c3658b57b2209e6c5a9af365a0702bfe5ec980
t-k- <clock126@126.com>
2020-12-04 23:05:16 +0800

    update

--- footer.vue
+++ footer.vue
@@ -94,7 +94,7 @@
 		  for supporting and encouraging me to work on the idea of creating a math-aware search engine.
 		  I feel grateful to my cloud provider
           <a target="_blank" href="https://linode.com/" rel="noopener">Linode</a> who offers a great
-          infrastructure to keep this site staying on real axis. Also, I want to shout out to
+          infrastructure to keep this site staying on real axis. Also I want to shout out to
           <a target="_blank" href="https://math.stackexchange.com/users/8297" rel="noopener">Martin Sleziak</a>
 		  for his very first donation to this project and all kinds of support. And shout to
           <a target="_blank" href="https://math.stackexchange.com/users/290240" rel="noopener">@Sil</a>
